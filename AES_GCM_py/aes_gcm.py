b8d86a0c67af3805c966971dcb9dd972121cf61a
柯均洁 <kjj3300@163.com>
2015-05-21 11:18:03 +0800

    new AES_GCM

--- AES_GCM_py/aes_gcm.py
+++ AES_GCM_py/aes_gcm.py
@@ -1,27 +1,4 @@
 #!/usr/bin/env python
-
-"""
-    Copyright (C) 2013 Bo Zhu http://about.bozhu.me
-
-    Permission is hereby granted, free of charge, to any person obtaining a
-    copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-    DEALINGS IN THE SOFTWARE.
-"""
-
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
 from Crypto.Util.number import long_to_bytes, bytes_to_long
@@ -181,28 +158,19 @@ class AES_GCM:
 
 
 if __name__ == '__main__':
-    master_key = 0xfeffe9928665731c6d6a8f9467308308
-    plaintext = b'\xd9\x31\x32\x25\xf8\x84\x06\xe5' + \
-                b'\xa5\x59\x09\xc5\xaf\xf5\x26\x9a' + \
-                b'\x86\xa7\xa9\x53\x15\x34\xf7\xda' + \
-                b'\x2e\x4c\x30\x3d\x8a\x31\x8a\x72' + \
-                b'\x1c\x3c\x0c\x95\x95\x68\x09\x53' + \
-                b'\x2f\xcf\x0e\x24\x49\xa6\xb5\x25' + \
-                b'\xb1\x6a\xed\xf5\xaa\x0d\xe6\x57' + \
-                b'\xba\x63\x7b\x39'
-    auth_data = b'\xfe\xed\xfa\xce\xde\xad\xbe\xef' + \
-                b'\xfe\xed\xfa\xce\xde\xad\xbe\xef' + \
-                b'\xab\xad\xda\xd2'
-    init_value = 0xcafebabefacedbaddecaf888
-    ciphertext = b'\x42\x83\x1e\xc2\x21\x77\x74\x24' + \
-                 b'\x4b\x72\x21\xb7\x84\xd0\xd4\x9c' + \
-                 b'\xe3\xaa\x21\x2f\x2c\x02\xa4\xe0' + \
-                 b'\x35\xc1\x7e\x23\x29\xac\xa1\x2e' + \
-                 b'\x21\xd5\x14\xb2\x54\x66\x93\x1c' + \
-                 b'\x7d\x8f\x6a\x5a\xac\x84\xaa\x05' + \
-                 b'\x1b\xa3\x0b\x39\x6a\x0a\xac\x97' + \
-                 b'\x3d\x58\xe0\x91'
-    auth_tag = 0x5bc94fbc3221a5db94fae95ae7121a47
+    master_key = 0x00000000000000000000000000000000
+    plaintext = b'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    auth_data = b''
+    init_value = 0x000000000000000000000000
+    ciphertext = b'\x42\xca\x99\x8a\x25\xf0\xe4\xda' + \
+                 b'\xba\x62\x89\xf5\x3c\xfc\xb1\x28' + \
+                 b'\xa6\xc7\xf9\xff\x1c\x1d\x0e\x7b' + \
+                 b'\xae\xa7\xe8\x9d\xf7\xef\xa4\x86' + \
+                 b'\x47\x6a\x78\x4b\x25\x1f\xf9\xb4' + \
+                 b'\x4f\xf9\xc7\xde\xa3\xe7\xde\x96' + \
+                 b'\xbe\x35\xdb\x63\x21\xbf\x1b\x4e' + \
+                 b'\x4f\x4b\x8a\x8b\xf1\xfa'
+    auth_tag = 0xd2752fd2c4310e79669058f1d6344c1f
 
     print 'plaintext:', hex(bytes_to_long(plaintext))
 
